23a639951fb4f2c03f2223334c16a9125b690607
Patryk Karny <patryk.karny@infullmobile.com>
2018-04-08 11:52:31 +0200

    Webhooks integration

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -12,6 +12,12 @@ module.exports = (app) => {
     res.send('Thank you for your feedback!');
   });
 
+  app.post('/api/surveys/webhooks', (req, res) => {
+    console.log(req.body);
+
+    res.send({});
+  });
+
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const {
       title,
